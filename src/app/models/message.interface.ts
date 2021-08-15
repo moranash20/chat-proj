e18b0f92813838dd1a79223b50349503727eec88
מורן אשכנזי - טולידנו <moranash20@gmail.com>
2021-08-15 11:44:51 +0300

    add room+message intefaces and add chat srvice

---
+++ src/app/models/message.interface.ts
@@ -0,0 +1,6 @@
+export interface IMessage {
+  id: string;
+  userId: string;
+  timeStamp: number;
+  body: string;
+}
---
+++ src/app/models/room.interface.ts
@@ -0,0 +1,8 @@
+import { IMessage } from './index';
+
+export interface IChatRoom {
+  id: string;
+  roomName: string;
+  messages: Array<IMessage>;
+  createdUserId: string;
+}
